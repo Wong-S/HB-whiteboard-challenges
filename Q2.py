5c78812860ab0f954d2d0568b4b691d4b147ff68
sarahwong <71740977+Wong-S@users.noreply.github.com>
2021-01-25 20:03:38 -0600

    Initial commit

---
+++ Q2.py
@@ -0,0 +1,46 @@
+
+#FizBuzz Problem: 
+
+# No input 
+# Test Case: Grab numbers through the whole range to test. Example, 15, etc 
+# I: 3
+# O: Fizz
+
+# I: 5
+# O: Buzz
+
+# I: 3 , 5
+# O: Fizzbuzz 
+
+#Thought/Pseudo:  Be a little shorter with pseudo 
+
+#Create a function (no parameters)
+#Iterate through range 1-20
+#Check if number is divisible 3 
+#output “fizz”
+
+#Check if number is divisible 5
+#output “buzz”
+
+#Check if number is divisible 3 and 5 
+#output fizzbuzz 
+
+#If none above
+#output num 
+
+#Code 
+
+Def fizzbuzz():
+	For num in range(1, 21):
+
+		if num % 3 == 0 and num % 5 == 0 :
+			print(“fizzbuzz”)
+
+		elif num % 3 == 0:
+			print(“fizz”)
+
+		Elif num % 5 == 0:
+			print(“buzz”)
+
+		Else:
+			print(num)
